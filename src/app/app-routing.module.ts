f40376e978ed34d67ea813b068ae2dfb2e7d08d2
GabeHill <gabelhill2@gmail.com>
2021-12-02 11:39:03 -0700

    routering

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { StuffComponent } from './stuff/stuff.component'
+import { ThingsComponent } from './things/things.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'stuff', component: StuffComponent },
+  { path: 'things', component: ThingsComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const ArrayOfComponents = [StuffComponent, ThingsComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, ArrayOfComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StuffComponent } from './stuff/stuff.component';
+import { ThingsComponent } from './things/things.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArrayOfComponents,
+    StuffComponent,
+    ThingsComponent
   ],
   imports: [
     BrowserModule,
